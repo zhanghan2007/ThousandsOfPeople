71da2b07f2cd78228c7e947aafbf3c671b8b5420
zhanghan <zhanghan@pook.cc>
2024-09-18 15:00:24 +0800

    千人同屏

---
+++ Assets/Scripts/GameApp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameApp : MonoBehaviour {
+
+    public GameObject archerPrefab = null;
+
+	// Use this for initialization
+	void Start () { // 20x40 --->800个小兵;
+        float z = 0;
+        for (int i = 0; i < 20; i++) {
+            float x = 0;
+            for (int j = 0; j < 40; j++) {
+                GameObject archer = GameObject.Instantiate(this.archerPrefab);
+                archer.transform.SetParent(this.transform, false);
+                archer.transform.position = new Vector3(x, 0, z);
+                x ++;
+            }
+            z += 1;
+        }
+	}
+}
